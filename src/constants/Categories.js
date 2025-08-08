4e3b7c82735db38925bce24a09b9cc3355af73e9
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-08-08 19:36:06 +0530

    cleaned thing added for test

--- src/constants/Categories.js
+++ src/constants/Categories.js
@@ -9,29 +9,5 @@ export const CATEGORIES = [
     subcategories: [
       'BoomParis',
     ],
-  },
-  {
-    name: 'Carousel',
-    subcategories: [
-      'Image Carousel',
-      'Product Carousel',
-      'Testimonial Carousel',
-    ],
-  },
-  {
-    name: 'Scroll Based',
-    subcategories: [
-      'Parallax Scroll',
-      'Reveal Animation',
-      'Sticky Navigation',
-    ],
-  },
-  {
-    name: 'Static',
-    subcategories: [
-      'Hero Section',
-      'Feature Grid',
-      'Pricing Table',
-    ],
   }
 ];
--- src/constants/Components.js
+++ src/constants/Components.js
@@ -2,27 +2,6 @@ const backgrounds = {
   'boomparis': () => import("@demo/Backgrounds/BoomParisDemo"),
 };
 
-const carousel = {
-  'image-carousel': () => import("@demo/Carousel/ImageCarouselDemo"),
-  'product-carousel': () => import("@demo/Carousel/ProductCarouselDemo"),
-  'testimonial-carousel': () => import("@demo/Carousel/TestimonialCarouselDemo"),
-};
-
-const scrollBased = {
-  'parallax-scroll': () => import("@demo/ScrollBased/ParallaxScrollDemo"),
-  'reveal-animation': () => import("@demo/ScrollBased/RevealAnimationDemo"),
-  'sticky-navigation': () => import("@demo/ScrollBased/StickyNavigationDemo"),
-};
-
-const staticComponents = {
-  'hero-section': () => import("@demo/Static/HeroSectionDemo"),
-  'feature-grid': () => import("@demo/Static/FeatureGridDemo"),
-  'pricing-table': () => import("@demo/Static/PricingTableDemo"),
-};
-
 export const componentMap = {
   ...backgrounds,
-  ...carousel,
-  ...scrollBased,
-  ...staticComponents,
 };
