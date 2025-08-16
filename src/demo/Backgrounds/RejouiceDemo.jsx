f876929e9f0b6e16d8c7c26694238ce5ba78d8d5
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-08-16 14:47:17 +0530

    added dependencies

--- src/demo/Backgrounds/RejouiceDemo.jsx
+++ src/demo/Backgrounds/RejouiceDemo.jsx
@@ -1,5 +1,6 @@
 import { CodeTab, PreviewTab, TabbedLayout } from "../../components/common/TabbedLayout";
 import CodeExample from "../../components/code/CodeExample";
+import Dependencies from "../../components/code/Dependencies";
 import FullScreenPreview from "../../components/common/Preview/FullScreenPreview";
 import { rejouice } from "../../constants/code/Backgrounds/rejouiceCode.js";
 import rejouiceVideo from '../../assets/videos/rejouice.mp4';
@@ -15,6 +16,7 @@ const RejouiceDemo = () => {
           loop={true}
           muted={true}
         />
+        <Dependencies dependencyList={['gsap', 'locomotive-scroll']} />
       </PreviewTab>
 
       <CodeTab>
